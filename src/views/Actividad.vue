e39b582a06884fc5b76bc6046f5360221a006716
Sebastian Trujillo <129899570+strujilloa@users.noreply.github.com>
2024-09-11 12:15:06 -0500

    Update 3

--- src/views/Actividad.vue
+++ src/views/Actividad.vue
@@ -7,15 +7,15 @@
       .col-6.col-md-4.col-lg-3.mb-4.mb-md-0(data-aos="fade-right")
         .tarjeta.h-100.d-flex.align-items-center.p-4
           figure
-            img(src="@/assets/template/falso-verdadero.svg", alt="Imagen decorativa")
+            img(src="@/assets/template/opcion-multiple.svg", alt="Imagen decorativa")
       .col-12.col-md-8.col-lg-9(data-aos="fade-left")
         .titulo-segundo
-          h2 Nombre_de_la_actividad
-        p.mb-4 Objetivo_actividad
+          h2 Validación documental
+        p.mb-4 Apropiar los concomimientos documentales dentro de una organización, incentivando al actuar del quehacer laboral del individuo.
         .tarjeta.actividad.p-3
           .row.justify-content-around.align-items-center            
             .col-sm.mb-3.mb-sm-0
-              p.fw-bold.mb-0 Tipo_de_actividad
+              p.fw-bold.mb-0 Opción múltiple
             .col-auto
               a.boton.boton--b(:href="obtenerLink('/actividades/actividad.html')" target="_blank")
                 span Realizar
